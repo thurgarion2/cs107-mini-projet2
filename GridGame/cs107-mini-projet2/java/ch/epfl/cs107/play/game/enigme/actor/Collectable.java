a3bd776160e1ce30bb8a187eb33a35dcfc207d13
thetorf <jean-daniel.rouveyrol@epfl.ch>
2018-12-03 13:06:02 +0100

    drop

--- GridGame/cs107-mini-projet2/java/ch/epfl/cs107/play/game/enigme/actor/Collectable.java
+++ GridGame/cs107-mini-projet2/java/ch/epfl/cs107/play/game/enigme/actor/Collectable.java
@@ -42,7 +42,7 @@ public abstract class Collectable extends AreaEntity {
 
     /**return true if possible to add a collectable to the area and add it */
 
-    public boolean deppose(Area area, DiscreteCoordinates position){
+    public boolean drop(Area area, DiscreteCoordinates position){
         this.setCurrentPosition(position.toVector());
         if(!area.canEnterAreaCells(this, this.getCurrentCells())){
             return false;
