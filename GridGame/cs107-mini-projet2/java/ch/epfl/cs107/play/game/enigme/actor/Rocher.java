334c5f6de967c412474faa1169c40efb372f76d5
thetorf <jean-daniel.rouveyrol@epfl.ch>
2018-12-07 13:39:17 +0100

    rock

--- GridGame/cs107-mini-projet2/java/ch/epfl/cs107/play/game/enigme/actor/Rocher.java
+++ GridGame/cs107-mini-projet2/java/ch/epfl/cs107/play/game/enigme/actor/Rocher.java
@@ -4,17 +4,17 @@ import ch.epfl.cs107.play.game.areagame.Area;
 import ch.epfl.cs107.play.game.areagame.actor.AreaEntity;
 import ch.epfl.cs107.play.game.areagame.actor.Orientation;
 import ch.epfl.cs107.play.game.areagame.actor.Sprite;
+import ch.epfl.cs107.play.game.enigme.actor.door.Gate;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.signal.Signal;
 import ch.epfl.cs107.play.window.Canvas;
 
 import java.util.LinkedList;
 import java.util.List;
 
-public class Rocher extends AreaEntity {
+public class Rocher extends Gate {
     private List<DiscreteCoordinates> currrentCells;
 
-    private boolean isActivate;
-    private Sprite sprite;
 
     /**
      * Default AreaEntity constructor
@@ -23,39 +23,23 @@ public class Rocher extends AreaEntity {
      * @param orientation (Orientation): Initial orientation of the entity in the Area. Not null
      * @param position    (DiscreteCoordinate): Initial position of the entity in the Area. Not null
      */
-    public Rocher(Area area, Orientation orientation, DiscreteCoordinates position, boolean isActivate, ) throws NullPointerException {
-        super(area, orientation, position);
+    public Rocher(Area area, Orientation orientation, String sprite, boolean isOpen,Signal signal, DiscreteCoordinates position ) throws NullPointerException {
+        super(area, orientation, sprite, sprite, isOpen ,position);
         currrentCells = new LinkedList<>();
         currrentCells.add(position);
 
-        this.isActivate = isActivate;
-        this.sprite = new Sprite("rock3", 1.0f, 1.0f, this);
-    }
-
-    @Override
-    public void update(float deltaTime) {
-        super.update(deltaTime);
-        if()
     }
 
     @Override
     public void draw(Canvas canvas) {
-        if(isActivate) {
-            sprite.draw(canvas);
+        if(getIsOpen()) {
+            getSprite().draw(canvas);
         }
     }
 
     @Override
     public List<DiscreteCoordinates> getCurrentCells() {
         return currrentCells;
-    }
-
-    @Override
-    public boolean takeCellSpace() {
-        if(isActivate){
-            return true;
-        }
-        return false;
     }
 
     @Override
